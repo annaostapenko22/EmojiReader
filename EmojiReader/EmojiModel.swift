905b1b515f51ca6ed62a960bb36d840d1d3c2c48
Anna Ostapenko <anna.ostapenko272@gmail.com>
2021-03-09 21:26:50 +0100

    added cells with basic functionality

---
+++ EmojiReader/EmojiModel.swift
@@ -0,0 +1,16 @@
+//
+//  EmojiModel.swift
+//  EmojiReader
+//
+//  Created by pc on 3/9/21.
+//  Copyright © 2021 pc. All rights reserved.
+//
+
+import Foundation
+
+struct Emoji {
+    var emoji: String
+    var name: String
+    var description: String
+    var isFavourite: Bool
+}
---
+++ EmojiReader/EmojiTableViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  EmojiTableViewCell.swift
+//  EmojiReader
+//
+//  Created by pc on 3/9/21.
+//  Copyright © 2021 pc. All rights reserved.
+//
+
+import UIKit
+
+class EmojiTableViewCell: UITableViewCell {
+    @IBOutlet weak var emojiLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+ 
+    
+}
